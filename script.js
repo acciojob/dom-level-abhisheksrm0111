e4c51f39229dd30c0b93e970196e63766b45afc7
abhisheksrm0111 <abhisheksrm0111@gmail.com>
2025-05-31 14:35:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+window.onload = function() {
+    let element = document.getElementById('level');
+    let count = 0;
+
+    while (element) {
+        count++;
+        element = element.parentElement;
+    }
+
+    alert("The level of the element is: " + count);
+};
